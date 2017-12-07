b27132bd688ceb2188680def4775991fc8d08671
James Austin Wehrwein <jawehrwein@gmail.com>
2017-12-07 08:52:49 -0600

    new theme and other changes

--- R/a_theme.R
+++ R/a_theme.R
@@ -44,10 +44,97 @@ a_theme<- function(
   aplot
 }
 
+a_robot_theme<- function(
+  base_family="Roboto Light",
+  base_size = 10,
+  plot_title_family='Roboto Slab',
+  plot_title_size = 16,
+  plot_title_face="plain",
+  subtitle_family=base_family,
+  subtitle_size = 10,
+  subtitle_face = "plain",
+  strip_text_family = base_family,
+  strip_text_size = 12,
+  strip_text_face = "plain",
+  caption_family= base_family,
+  caption_size = 10,
+  caption_face = "plain",
+  axis_text_size = base_size,
+  axis_title_family = base_family,
+  axis_title_size = 10,
+  axis_title_face = "plain",
+  grid_col='#dedede',
+  grid=TRUE,
+  noaxis=FALSE,
+  emphasis='xy',
+  rotate_x=FALSE)
+{
+  aplot <- ggplot2::theme_minimal(base_family=base_family, base_size=base_size)
+  
+  if (inherits(grid, "character") | grid == TRUE) {
+    
+    aplot <- aplot + theme(panel.grid=element_line(color=grid_col, size=0.2))
+    aplot <- aplot + theme(panel.grid.major=element_line(color=grid_col, size=0.2))
+    aplot <- aplot + theme(panel.grid.minor=element_line(color=grid_col, size=0.15))
+    
+    
+    if (inherits(grid, "character")) {
+      if (regexpr("X", grid)[1] < 0) aplot <- aplot + theme(panel.grid.major.x=element_blank())
+      if (regexpr("Y", grid)[1] < 0) aplot <- aplot + theme(panel.grid.major.y=element_blank())
+      if (regexpr("x", grid)[1] < 0) aplot <- aplot + theme(panel.grid.minor.x=element_blank())
+      if (regexpr("y", grid)[1] < 0) aplot <- aplot + theme(panel.grid.minor.y=element_blank())
+    }
+    
+  } else {
+    aplot <- aplot + theme(panel.grid=element_blank())
+  }
+  if (inherits(grid, "character") | rotate_x == TRUE) {
+    aplot<-aplot+theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  } 
+  
+  aplot <- aplot + theme(legend.background=element_blank())
+  aplot <- aplot + theme(legend.key=element_blank())
+  if (inherits(grid, "character") | noaxis == TRUE) {
+    aplot <- aplot + theme(axis.text.x=element_blank())
+    aplot <- aplot + theme(axis.text.y=element_blank())}
+  else{
+    aplot <- aplot + theme(axis.text.x=element_text(size=axis_text_size,family=subtitle_family,face = dplyr::case_when(emphasis=='x' ~'bold',emphasis=='y'~'plain',emphasis=='xy'~'plain'),color='#444444'))
+    aplot <- aplot + theme(axis.text.y=element_text(size=axis_text_size,family=subtitle_family,face = dplyr::case_when(emphasis=='y' ~'bold',emphasis=='x'~'plain',emphasis=='xy'~'plain'),color='#444444'))}
+  aplot <- aplot + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
+  aplot <- aplot + theme(axis.title.x=element_text(size=axis_title_size, family=axis_title_family, face=axis_title_face, margin=margin(t = 18, b = 10, unit = "pt")))
+  aplot <- aplot + theme(axis.title.y=element_text(size=axis_title_size, family=axis_title_family, face=axis_title_face,margin=margin(r = 18, unit = "pt")))
+  aplot <- aplot + theme(strip.text=element_text(hjust=0, size=strip_text_size,face=strip_text_face, family=strip_text_family))
+  aplot <- aplot + theme(panel.spacing=grid::unit(2, "lines"))
+  aplot <- aplot + theme(plot.title=element_text(hjust=0, size=plot_title_size, family=plot_title_family, face=plot_title_face,color='#444444'))
+  aplot <- aplot + theme(plot.subtitle=element_text(hjust=0, size=subtitle_size, margin=margin(t = 8, b = 12, unit = "pt"),family=subtitle_family, face=subtitle_face,color='#565656', lineheight=1))
+  aplot <- aplot + theme(plot.caption=element_text(hjust=1, size=caption_size,  family=caption_family, face=caption_face,color='#565656'))
+  aplot
+  
+}
 
-#fun color palette
+#fun color palette #1
 a_palette <- c('#2A363B','#019875','#99B898','#FECEA8','#FF847C','#E84A5F','#C0392B','#96281B')
 a_pal <- function() { scales::manual_pal(a_palette) }
-a_scale_color <- function(...) { discrete_scale("color", "a", a_pal(), ...) }
+a_scale_colour <- function(...) { discrete_scale("colour", "a", a_pal(), ...) }
+a_scale_color<-a_scale_colour
 a_scale_fill <- function(...) { discrete_scale("fill", "a", a_pal(), ...) }
 
+#reversed color palette
+a_rpal <- function() { scales::manual_pal(rev(a_palette)) }
+a_reversed_colour<- function(...) { discrete_scale("colour", "a", a_rpal(), ...) }
+a_reversed_color<-a_reversed_colour
+a_reversed_fill <- function(...) { discrete_scale("fill", "a", a_rpal(), ...) }
+
+#stepped palette 4
+a_spalette<-c('#2A363B','#99B898','#FF847C','#C0392B')
+a_spal <- function() { scales::manual_pal(a_spalette) }
+a_step_colour <- function(...) { discrete_scale("colour", "a", a_spal(), ...) }
+a_step_color<-a_step_colour
+a_step_fill<-function(...) { discrete_scale("fill", "a", a_spal(), ...) }
+
+
+
+#for use with axis labels, returns absolute value with commas
+abs_comma <- function (x, ...) {
+  format(abs(x), ..., big.mark = ",", scientific = FALSE, trim = TRUE)
+}
--- README.md
+++ README.md
@@ -1,14 +1,26 @@
-# awtools
+---
+title: "awtools"
+output:
+  html_document:
+    keep_md: true
+  md_document:
+    variant: markdown_github
+---
 # a_theme
-A simple, distilled, #rstats theme used mainly on [www.austinwehrwein.com](http://austinwehrwein.com/)
+A simple, distilled, #rstats theme used mainly on [www.austinwehrwein.com](https://austinwehrwein.com/)
 
-This theme uses fonts that can be found and downloaded from Google Fonts:
+The original <code>a_theme</code> uses fonts that can be found and downloaded from Google Fonts:
 
  - [Slabo 27px](https://fonts.google.com/specimen/Slabo+27px) (plot title, legend)
  - [PT Sans Narrow](https://fonts.google.com/specimen/PT+Sans+Narrow) (subtitle, captions, axis_text)
  
+The new <code>a_robot_theme</code> uses fonts that can be found and downloaded from Google Fonts:
+
+ - [Roboto Slab](https://fonts.google.com/specimen/Roboto+Slab) (plot title, legend)
+ - [Roboto Light](https://fonts.google.com/specimen/Roboto) (subtitle, captions, axis_text)
+ 
  # Examples
-Here is a simple scatterplot
+Here is a simple scatterplot with the original <code>a_theme</code>.
 
 ```r
 #devtools::install_github('awhstin/awtools')
@@ -25,8 +37,50 @@ ggplot(heightweight,aes(x=ageYear,y=heightIn,color=ageYear))+
 ```
 
 ![](README_files/figure-html/unnamed-chunk-1-1.png)<!-- -->
+
+Here is a simple scatterplot with a revised version using specifically Roboto series fonts <code>a_robot_theme</code>. Along with some added elements, sizing, and other aesthetic fixes this will be used most on my website. 
+
+```r
+#devtools::install_github('awhstin/awtools')
+library(awtools)
+library(gcookbook)
+library(ggplot2)
+library(dplyr)
+```
+
+```
+## Warning: package 'dplyr' was built under R version 3.4.2
+```
+
+```
+## 
+## Attaching package: 'dplyr'
+```
+
+```
+## The following objects are masked from 'package:stats':
+## 
+##     filter, lag
+```
+
+```
+## The following objects are masked from 'package:base':
+## 
+##     intersect, setdiff, setequal, union
+```
+
+```r
+ggplot(heightweight,aes(x=ageYear,y=heightIn,color=ageYear))+
+  geom_point()+
+  a_robot_theme()+
+  labs(title='Height by Age',
+       subtitle='Sample data of height in inches by age in years.',
+       caption='Source: R Graphics Cookbook')
+```
+
+![](README_files/figure-html/unnamed-chunk-2-1.png)<!-- -->
  
-Here is an example of the color palette
+Here is an example of the color palette.
 
 ```r
 ggplot(uspopage, aes(x=Year, y=Thousands, fill=AgeGroup)) + 
@@ -40,5 +94,42 @@ ggplot(uspopage, aes(x=Year, y=Thousands, fill=AgeGroup)) +
   theme(legend.position="bottom")
 ```
 
-![](README_files/figure-html/unnamed-chunk-2-1.png)<!-- -->
+![](README_files/figure-html/unnamed-chunk-3-1.png)<!-- -->
+
+I also created a reverse of the color palette.
+
+```r
+ggplot(uspopage, aes(x=Year, y=Thousands, fill=AgeGroup)) + 
+  geom_area() +
+  a_reversed_fill() +
+  scale_y_continuous(labels = scales::comma)+
+  labs(title="Age distribution of population\nin the U.S., 1900-2002",
+       subtitle="Example data from the R Graphics Cookbook.",
+       caption="Source: R Graphics Cookbook") +
+  a_robot_theme() +
+  theme(legend.position="bottom")
+```
+
+![](README_files/figure-html/unnamed-chunk-4-1.png)<!-- -->
+
+Finally for work I compare 4 and 6 factors often so I created a stepped version of my color palette. 
+
+I also created a reverse of the color palette.
+
+```r
+groups<-unique(uspopage$AgeGroup)
+uspopage<-uspopage%>%
+  filter(uspopage$AgeGroup %in% head(groups,4))
+ggplot(uspopage, aes(x=Year, y=Thousands, fill=AgeGroup)) + 
+  geom_area() +
+  a_step_fill() +
+  scale_y_continuous(labels = scales::comma)+
+  labs(title="Age distribution of population\nin the U.S., 1900-2002",
+       subtitle="Example data from the R Graphics Cookbook.",
+       caption="Source: R Graphics Cookbook") +
+  a_robot_theme() +
+  theme(legend.position="bottom")
+```
+
+![](README_files/figure-html/unnamed-chunk-5-1.png)<!-- -->
 
